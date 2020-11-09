4ad7146b2b6069d4b4eeccd98d0a572fcc213081
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-11-09 19:40:25 -0300

    first commit

---
+++ WoopSicredi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WoopSicredi
+//
+//  Created by Alexandre Scheer Bing on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
