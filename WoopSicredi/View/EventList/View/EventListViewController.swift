fcfcc1f5bc82fa9b5a9561d77dd1f10808943b03
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-11-11 07:05:36 -0300

    Organised code on EventListViewController

--- WoopSicredi/View/EventList/View/EventListViewController.swift
+++ WoopSicredi/View/EventList/View/EventListViewController.swift
@@ -25,11 +25,20 @@ class EventsListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
+        setupTableView()
+    }
+    
+    private func setupNavBar() {
         navigationItem.title = "Eventos"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupTableView() {
         tableView.tableFooterView = UIView()
         tableView.contentInsetAdjustmentBehavior = .never
         tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
         viewModel.fetchEventViewModel().observe(on: MainScheduler.instance).bind(to: tableView.rx.items){ (tableView, row, item) -> UITableViewCell in
             let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventListTableViewCell
             cell.setData(title: item.title, imageURL: item.imageURL)
@@ -41,7 +50,6 @@ class EventsListViewController: UIViewController {
             .subscribe(onNext: { [self] value in
                 coordinator?.goToEventDetail()
             }).disposed(by: disposeBag)
-
     }
     
     //TODO: Remove after all event's info are being displayed correctly in tableview
